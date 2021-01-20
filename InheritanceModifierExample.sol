48f12f94e5d0e6838a2fbeb68f9d4f9fbdff78c5
prathibhabilagi <31176835+prathibhabilagi@users.noreply.github.com>
2021-01-20 19:19:37 +0530

    Making use of inheritance  and modifier to authenticate the right owner to create, burn, purchase and send token

---
+++ InheritanceModifierExample.sol
@@ -0,0 +1,45 @@
+pragma solidity ^0.5.17;
+
+contract Owned {
+    address owner;
+    
+    constructor() public {
+        owner = msg.sender;
+    }
+    
+    modifier onlyOwner() {
+        require(msg.sender == owner, "You are not allowed");
+        _;
+    }
+}
+
+contract InheritanceModifierExample is Owned {
+   
+   mapping(address => uint) public tokenBalance;
+   
+   uint tokenPrice = 1 ether;
+   
+   constructor() public {
+       tokenBalance[owner] = 100;
+   }
+   
+   function createNewToken() public  onlyOwner{
+       tokenBalance[owner]++;
+   }
+   
+   function burnToken() public onlyOwner {
+       tokenBalance[owner]--;
+   }
+   
+   function purchaseToken() public payable {
+       require((tokenBalance[owner] * tokenPrice) / msg.value > 0, "not enough tokens");
+       tokenBalance[owner] -= msg.value / tokenPrice;
+       tokenBalance[msg.sender] += msg.value / tokenPrice;
+   }
+   
+   function sendToken(address _to, uint _amount) public {
+       require(tokenBalance[msg.sender] >= _amount, "not enough Tokens");
+       assert(tokenBalance[_to] + _amount >= tokenBalance[_to]);
+       assert(tokenBalance[msg.sender] - _amount <= tokenBalance[msg.sender]);
+   }
+}
