808d6a35bae4cdccf962654270ccc8ed0efd9e28
prathibhabilagi <31176835+prathibhabilagi@users.noreply.github.com>
2021-01-22 19:05:32 +0530

    To check isOwner and add money or reduce money from the allowance

---
+++ SimpleWallet/Allowance .sol
@@ -0,0 +1,27 @@
+pragma solidity ^0.5.11;
+
+import 'https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/ownership/Ownable.sol';
+import 'https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/math/SafeMath.sol';
+
+contract Allowance is Ownable {
+    
+    using SafeMath for uint;
+    event AllowanceChanged(address indexed _forWho, address indexed _fromWhom, uint _oldAmount, uint _newAmount);
+    
+    mapping(address => uint) public allowance;
+  
+    function addAllowance(address _who, uint _amount) public {
+        emit AllowanceChanged(_who, msg.sender, allowance[_who], _amount);
+        allowance[_who] = _amount;
+    }
+    
+    modifier ownerOrAllowed(uint _amount) {
+        require(isOwner() || allowance[msg.sender] >= _amount, "You are not allowed");
+        _;
+    }
+    
+    function reduceAllowance(address _who, uint _amount) internal {
+        emit AllowanceChanged(_who, msg.sender, allowance[_who], allowance[_who].sub(_amount));
+        allowance[_who] = allowance[_who].sub(_amount);
+    }
+}
